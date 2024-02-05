1da7370951cfeaae5f62f94f13c456d38d3eba8b
Harold Garcia <garciavargas9112@gmail.com>
2024-02-05 17:04:32 -0500

    Implementando react

--- src/App.js
+++ src/App.js
@@ -1,24 +1,35 @@
-import logo from './logo.svg';
-import './App.css';
+import Error404 from 'containers/errors/Error404';
+import Home from 'containers/pages/Home';
+import store from './store';
+import { Provider } from 'react-redux';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
+import Blog from 'containers/pages/blog/Blog';
+import BlogPost from 'containers/pages/blog/BlogPost';
+import BlogCategory from 'containers/pages/blog/category/BlogCategory';
+import Search from 'containers/pages/Search';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Provider store={store}>
+      <Router>
+        <Routes>
+          {/* Error Display */}
+          <Route path="*" element={<Error404/>}/>
+
+          {/* Home Display */}
+          <Route path="/" element={<Home/>}/>
+
+          <Route path="/blog" element={<Blog/>}/>
+          <Route path="/blog/post/:slug" element={<BlogPost/>}/>
+          <Route path="/blog/categories/:category_id" element={<BlogCategory/>}/>
+
+          <Route path="/search/:term" element={<Search/>}/>
+
+
+        </Routes>
+      </Router>
+    </Provider>
   );
 }
 
